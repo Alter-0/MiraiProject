13ddf95e1f9b9f3966cc75bbbc45cd34a13c2877
alter <453749982@qq.com>
2020-12-15 10:15:18 +0800

    完善新番详情页面

--- animate/detail.php
+++ animate/detail.php
@@ -8,7 +8,7 @@
         .circle_process{
 
             position: relative;
-            top:0px;
+            top:-10px;
             width: 199px;
             height : 200px;
         }
@@ -258,7 +258,7 @@
         }
         .score .scorenumber{
             position:relative ;
-            top:-120px
+            top:-130px
 
         }
         .score .scorenumber .scoretext{
@@ -299,23 +299,31 @@
 
         }
         .btns{
-            text-align: center;
 
             float: left;
-            width:128px;
-            height: 48px;
-            background-color: #f36392;
+            display: inline-block;
             position: absolute;
-            border-radius: 8px;
-            cursor: pointer;
+
             top:300px;
             left: 300px;
-            color: #ffffff;
-            font-size: 14px;
+
+        }
+        .btn_like:hover{
+            background-color: rgb(255,133,173);
         }
         .btn_like{
             vertical-align: center;
             display: inline-block;
+            border-radius: 8px;
+            cursor: pointer;
+            text-align: center;
+            line-height: 50px;
+            color: #ffffff;
+            font-size: 16px;
+            transition: all .3s ease 0s;
+            width:128px;
+            height: 48px;
+            background-color: #f36392;
         }
         .btn_like i{
             display: inline-block;
@@ -326,24 +334,124 @@
             background-image: url(http://s1.hdslb.com/bfs/static/review/media/asserts/heart-bangumi.svg);
             background-repeat: no-repeat;
         }
-        .top h1{
-            line-height: 300px;
-            color: rgba(255, 255, 255, 0.8);
-            font-size: 80px;
-            font-family: 幼圆;
-            vertical-align: center;
-            text-align: left;
-            margin: 0 10%;
-            position: relative;
-            text-shadow: 10px 10px 20px rgba(0,0,0,0.8);
+        .btn_liked{
+
+            line-height: 42px;
+            background-color: hsla(0,0%,100%,0.12);
+            border: 4px solid hsla(0,0%,100%,0.5);
+            text-shadow: 0 0 4px rgba(0,0,0,0.6);
+        }
+        .btn_liked:hover{
+            background-color: hsla(0,0%,100%,0.12);
+        }
+        .btn_liked i{
+            vertical-align: -6px;
+            background-image: url(http://s1.hdslb.com/bfs/static/review/media/asserts/icons.png);
+            background-position: -660px -1938px;
+
         }
 
+        .tab_nav{
+            position: relative;
+            height: 60px;
+            /*background-color: #FFFFFF;*/
+            background: rgba(255,255,255,0.8);
+            border-radius: 15px;
+            box-shadow: 5px 5px 20px 5px rgba(0,0,0,0.3);
+            top:-40px;
+            width: 80%;
+            margin: 0 10% 11px 10%;
+        }
+        .tab_nav ul{
+            font-size: 16px;
+            margin-left: 40px;
+
+            position: relative;
+            top:-3px;
+        }
+        .clearfix{
+
+        }
+
+        .tab_nav ul li{
+            cursor: pointer;
+            float: left;
+            padding: 0 6px 0px 6px;
+            color: #222;
+            transition: all 0.1s linear;
+            border-bottom: 3px solid rgba(0,0,0,0);
+            height: 60px;
+            line-height:60px;
+            /*position: relative;*/
+            /*top:60px;*/
+        }
+        .tab_nav ul li+li{
+            margin-left:28px ;
+        }
+
+        .tab_nav ul li.on, .tab_nav ul li :hover{
+            color: #00a1d6;
+            border-bottom-color:#00a1d6 ;
+        }
+        .clearfix:after{
+
+            content: "";
+            display: block;
+            width: 0;
+            height: 0;
+            clear: both;
+        }
+
+
+
+
         .content{
+            position: relative;
+            top: -40px;
             width: auto;
             height: 2000px;
-            margin: 30px 10% 200px 10%;
+            margin: 15px 10% 200px 10%;
+
+        }
+
+        .detail_card{
+            height: 100%;
+            width: 70.7%;
+            /*margin-right: 15px;*/
+            /*min-width: 1078px;*/
+            text-align: left;
+            float: left;
             background: rgba(255,255,255,0.8);
-            text-align: center;
+            box-sizing: border-box;
+            border-radius: 15px;
+            box-shadow: 5px 5px 20px 5px rgba(0,0,0,0.3);
+        }
+        .actors_card{
+            font-family: 幼圆, serif;
+            width:27.3%;
+            /*min-width: 415px;*/
+            text-align: left;
+            float: right;
+            padding:25px 20px 25px 20px;
+            line-height: 30px;
+            font-size: 15px;
+            background: rgba(255,255,255,0.8);
+            box-sizing: border-box;
+            border-radius: 15px;
+            box-shadow: 5px 5px 20px 5px rgba(0,0,0,0.3);
+        }
+        .actors_title{
+            font-family: 幼圆, serif;
+            font-size:18px;
+            font-weight: 700;
+        }
+        .staff_card{
+            width:27.3%;
+            /*min-width: 415px;*/
+            text-align: left;
+            float: right;
+            margin-top: 15px;
+            background: rgba(255,255,255,0.8);
             box-sizing: border-box;
             border-radius: 15px;
             box-shadow: 5px 5px 20px 5px rgba(0,0,0,0.3);
@@ -360,12 +468,6 @@
         .footer {
             width: 100%;
             height: 165px;
-        }
-
-        li{
-            line-height: 250px;
-            text-align: left;
-            margin-left: 5%;
         }
 
 
@@ -425,18 +527,43 @@
                 </div>
 
                 <div class="btns">
-                    <div class="like">
-                        <div class="btn_like">
+                    <div class="btn_like btn_liked">
                             <i></i>
-                                追番
-                        </div>
+                                已追番
                     </div>
                 </div>
             </div>
             <img id="bg" src="http://i0.hdslb.com/bfs/bangumi/image/4179b4398bad6f92e876e352cae21be7b8ceb8bf.png">
         </div>
+        <div class="tab_nav">
+            <ul  class="clearfix" >
+                <li class="on">番剧详情</li>
+                <li>相关视频</li>
 
+            </ul>
+
+        </div>
         <div class="content">
+            <div class="detail_card">
+                简介：大正时期，日本。心地善良的卖炭少年·炭治郎，有一天他的家人被鬼杀死了。而唯一幸存下来的妹妹——祢豆子变成了鬼。被绝望的现实打垮的炭治郎，为了寻找让妹妹变回人类的方法，决心朝着“鬼杀队”的道路前进。人与鬼交织的悲哀的兄妹的故事，现在开始！
+
+            </div>
+            <div class="actors_card">
+                <div class="actors_title">
+                    角色声优
+                </div>
+                <div calss="actors_text">
+                    <p>灶门炭治郎：花江夏树<br>灶门祢豆子：鬼头明里<br>我妻善逸：下野纮<br>嘴平伊之助：松冈祯丞<br>富冈义勇：樱井孝宏<br>鳞泷左近次：大冢芳忠<br>锖兔：梶裕贵<br>真菰：加隈亚衣<br>不死川玄弥：冈本信彦<br>产屋敷耀哉：森川智之<br>产屋敷辉利哉：悠木碧<br>产屋敷雏衣：井泽诗织<br>钢铁冢萤：浪川大辅<br>鎹鸦：山崎巧<br>佛堂鬼：绿川光<br>手鬼：子安武人</p>
+                </div>
+            </div>
+            <div class="staff_card">
+                <div class="staff_title">
+                    STAFF
+                </div>
+                <div class="staff_text">
+                    <p>原作：吾峠呼世晴（集英社《周刊少年JUMP》连载）<br>监督：外崎春雄<br>角色设计：松岛晃<br>副角色设计：佐藤美幸、梶山庸子、菊池美花<br>脚本制作：ufotable<br>概念美术：卫藤功二、矢中胜、竹内香纯、桦泽侑里<br>摄影监督：寺尾优一<br>3D监督：西胁一树<br>色彩设计：大前祐子<br>剪辑：神野学<br>音乐：梶浦由记、椎名豪<br>制作人：近藤光<br>动画制作：ufotable</p>
+                </div>
+            </div>
 
         </div>
 
